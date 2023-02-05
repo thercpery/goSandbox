446dfad74dffbd86ed3ba9569881a604184b239b
Roy Christian Pery <thercpery@gmail.com>
2023-02-05 15:50:11 +0800

    created saveToFile function

--- cards/main.go
+++ cards/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
 
@@ -10,7 +8,7 @@ func main() {
 	hand.print()
 	remainingCards.print()
 
-	fmt.Println(cards.toString())
+	cards.saveToFile("myCards")
 
 	// greeting := "Hi there!"
 	// fmt.Println([]byte(greeting)) // returns [72 105 32 116 104 101 114 101 33]
