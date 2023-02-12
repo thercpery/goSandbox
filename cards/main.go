30cbc59494de394dcd0d2d23178fa3e27eb59f58
Roy Christian Pery <thercpery@gmail.com>
2023-02-12 12:10:37 +0800

    created shuffle function

--- cards/main.go
+++ cards/main.go
@@ -13,6 +13,10 @@ func main() {
 	cardsFromFile := newDeckFromFile("myCards")
 	cardsFromFile.print()
 
-	// greeting := "Hi there!"
-	// fmt.Println([]byte(greeting)) // returns [72 105 32 116 104 101 114 101 33]
+	cards.shuffle()
+	cards.print()
+
+	// greeting := "I wish I was born in Norway"
+	// fmt.Println([]byte(greeting)) // returns [73 32 119 105 115 104 32 73 32 119 97 115 32 98 111 114 110 32 105 110 32 78 111 114 119 97 121]
+
 }
