d73811b8818d9e8c1e12826ec97eaaae40effbef
Roy Christian Pery <thercpery@gmail.com>
2023-02-15 21:19:49 +0800

    created ContactInfo struct

--- structs/main.go
+++ structs/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type ContactInfo struct {
+	email   string
+	zipCode int
+}
+
 type Person struct {
 	firstName string
 	lastName  string
+	contact   ContactInfo
 }
 
 func main() {
@@ -12,4 +18,21 @@ func main() {
 		firstName: "Alex",
 		lastName:  "Anderson"}
 	fmt.Println(alex)
+
+	var bob Person
+	bob.firstName = "Bob"
+	bob.lastName = "Smith"
+	fmt.Println(bob)
+	fmt.Printf("%+v", bob)
+
+	jim := Person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: ContactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 87000,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 }
