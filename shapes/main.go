8c63437aaf9c23034c6c9f09dfac154147b673ed
Roy Christian Pery <thercpery@gmail.com>
2023-02-19 13:20:20 +0800

    created shapes directory

---
+++ shapes/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+func main() {
+	t := triangle{
+		base:   2,
+		height: 3,
+	}
+
+	s := square{
+		sideLength: 5,
+	}
+
+	printArea(t)
+	printArea(s)
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
