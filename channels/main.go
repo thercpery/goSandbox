f9d8b21c7a6b666658823c6de068aa40bf702187
Roy Christian Pery <thercpery@gmail.com>
2023-02-20 13:33:24 +0800

    made checkLink to pass the link to a channel and made a function literal to get statuses every 5 seconds

--- channels/main.go
+++ channels/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 	links := []string{
-		"http://google.com",
+		"https://google.com",
 		"https://facebook.com",
 		"https://stackoverflow.com",
 		"https://go.dev",
@@ -21,17 +22,23 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	fmt.Println(<-c)
+	for l := range c {
+		// Function literal is an unnamed function used to wrapped chunk of code that will be executed at some point. It is the same as an anonymous function in Javascript
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link + "might be down!")
-		c <- "Might be down, I think."
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep, it's up!"
+	c <- link
 }
